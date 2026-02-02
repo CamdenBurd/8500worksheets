30aafbeedd8e542a889e1d6bc21d3b3bc31a257d
Camden Burd <cburd@clemson.edu>
2026-02-02 16:52:34 -0500

    added notes from class (February 2)

--- notes.r
+++ notes.r
@@ -24,3 +24,53 @@ library(DigitalMethodsData)
 data(gayguides)
 head(gayguides)
 mean(gayguides$Year)
+data(gayguides)
+
+
+#subsetting is giving directions within R. There are roughly three subsetting patterns.
+## by position 
+## by logic (using true and false values)
+
+Everything between 1970 and 1979
+year_1970s <- gayguides$Year <= 1970 & gayguides$Year <= 1979
+head(year_1970s)
+gayguides[year_1970s,]
+
+gayguides$city
+gayguides$city[-2]
+
+## using that money sign to call to specific variables
+
+## piping....... %>%
+
+help(sort)
+library(magrittr)
+
+#Using piping in class
+gayguides$state %>%
+unique() %>%
+sort() %>%
+head(10)
+
+library(DigitalMethodsData)
+data(DigitalMethodsData)
+data(BostonWomenVoters)
+
+Find all women whose occupation was housewife. 
+How many were there? And what was the age range?
+
+housewife <- BostonWomenVoters$Occupation == "housewife"
+housewife
+count(housewife)
+
+housewife <- BostonWomenVoters$Occupation == "Housewife"
+BostonWomenVoters(housewife)
+BostonWomenVoters[housewife, ]
+BostonWomenVoters[housewife,]
+BostonWomenVoters[housewife,]
+count(BostonWomenVoters[housewife,])
+houswives <- BostonWomenVoters[housewife,]
+#3167
+range(houswives$Age)
+length(houswives)
+length(houswives$Occupation)
